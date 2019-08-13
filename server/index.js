f015e3e9c6fc41de8127d5b3ed5db4132c0f57ff
HanQ <229075284@qq.com>
2019-08-13 18:37:44 +0800

    webpack-dev-middleware integrated

--- server/index.js
+++ server/index.js
@@ -1,124 +1,82 @@
+var fs = require("fs");
 const path = require('path')
-const webpack = require('webpack')
-const koaWebpack = require('koa-webpack');
-var MemoryFileSystem = require("memory-fs");
+const Koa = require('koa')
+
+const __DEV__ = process.env.NODE_ENV !== 'production'
+
+// const koaWebpack = require('koa-webpack');
+
+
+// var cfs = require("../cfs");
 // let serverMiddleware, clientMiddleware
-var webpackDevMiddleware = require('webpack-dev-middleware')
 
-const clientCompiler = webpack(require('../webpack.client.config.js'))
-const clientFs = new MemoryFileSystem();
-// clientCompiler.outputFileSystem = clientFs
-const clientMiddleware = webpackDevMiddleware(clientCompiler, { fs: clientFs, index: '../index.html',serverSideRender:true })
-const koaClientMiddleware = async (ctx, next) => { await clientMiddleware(ctx.req, ctx.res, next) }
-
-const serverCompiler = webpack(require('../webpack.server.config.js'))
-const serverFs = new MemoryFileSystem();
-// serverCompiler.outputFileSystem = serverFs
-const serverMiddleware = webpackDevMiddleware(serverCompiler, { fs: serverFs, index: '../index.html',serverSideRender:true })
-const koaServerMiddleware = async (ctx, next) => { await serverMiddleware(ctx.req, ctx.res, next) }
-
-// const waitWepbackReady=Promise.all([clientMiddleware(ctx.req, ctx.res, next),serverMiddleware(ctx.req, ctx.res, next)])
-
-// const waitWepbackReady=Promise.all([
-//   new Promise(resolve=>{
-//     serverCompiler.watch({},()=>{
-//       resolve()
-//     })
-//   }),
-//   new Promise(resolve=>{
-//     clientCompiler.watch({},()=>{
-//       resolve()
-//     })
-//   })
-// ])
-// const getWebpackMiddleware = Promise.all([
-//   koaWebpack({
-//     devMiddleware: {
-//       publicPath: '/server',
-//       serverSideRender: true
-//     },
-//     compiler: serverCompiler,
-//     hotClient: false,
-//     config: {}
-//   }),
-//   koaWebpack({
-//     devMiddleware: {
-//       publicPath: '/client',
-//       serverSideRender: true
-//     },
-//     hotClient: {},
-//     compiler: clientCompiler,
-//     config: {}
-//   })
-// ])
-
-Promise.resolve().then(function () {
-  // app.use(koaStatic(path.join(__dirname, '../dist'), {
-  //   defer: true
-  // }))
-  app.use(koaClientMiddleware)
-  app.use(koaServerMiddleware)
-  // app.use(async function (ctx, next) {
-  //   ctx.clientWebpackStats = clientCompiler.toJson()
-  //   ctx.serverWebpackStats = serverCompiler.toJson()
-  //   await next()
-  // })
-  app.use(async function (ctx) {
-    // const { default: Adapter, createStore } = require('../dist/server/index')
-    // const assetsByChunkName = ctx.state.webpackStats.toJson().assetsByChunkName;
-    console.log(ctx.res.locals.webpackStats.toJson())
-    if (true) creturn
-    if (!path.extname(ctx.url)) {
-      const serverData = await getData()
-      const store = createStore({
-        saveData: serverData
-      })
-      let html = fs.readFileSync(path.join(__dirname, '../dist/index.html'), 'utf-8')
-      // const modules = []
-
-      const nodeExtractor = new ChunkExtractor({
-        statsFile: path.join(__dirname, '../dist/server/loadable-stats.json'),
-        entrypoints: 'index',
-        publicPath: './client/'
-      })
-
-      // const extractor = new ChunkExtractor({ statsFile: path.join(__dirname, '../dist/loadable-stats.json') })
-      // const jsx = createWrapperWithApp({
-      //   store,
-      //   modules,
-      //   req: ctx.req,
-      //   extractor
-      // })
-      const jsx = nodeExtractor.collectChunks(React.createElement(Adapter, { store, req: ctx.req }, null))
-      html = html
-        .replace('<!-- reactComponents -->', ReactDOMServer.renderToString(jsx))
-        .replace('<!-- ssrData -->', JSON.stringify({ saveData: serverData }))
-        .replace('<!-- ssr_Scirpts -->', nodeExtractor.getScriptTags())
-        .replace('<!-- ssr_Css -->', nodeExtractor.getStyleTags())
-      // console.log(html)
-      // console.log(nodeExtractor.getScriptTags())
-
-      ctx.status = 200
-      ctx.set('content-type', 'text/html')
-      ctx.body = html
-    }
-  })
-
-  app.listen('8080', function () {
-    console.log('listen at 8080')
-  })
+const _res = (context) => ({
+  end: (content) => {
+    context.body = content;
+  },
+  getHeader: context.get.bind(context),
+  setHeader: context.set.bind(context),
 })
 
+let serverMiddleware, koaServerMiddleware, clientFs = fs
+if (__DEV__) {
+  const webpack = require('webpack')
+  const webpackDevMiddleware = require('webpack-dev-middleware')
+  const MemoryFileSystem = require("memory-fs");
+  const clientCompiler = webpack(require('../webpack.client.config.js'))
+  clientFs = new MemoryFileSystem();
+  // clientCompiler.outputFileSystem = clientFs
+  const clientMiddleware = webpackDevMiddleware(clientCompiler, { fs: clientFs, publicPath: '/client/' })
+  koaClientMiddleware = async (ctx, next) => { await clientMiddleware(ctx.req, _res(ctx), next) }
 
+  const serverCompiler = webpack(require('../webpack.server.config.js'))
+  // const serverFs = new MemoryFileSystem();
+  // serverCompiler.outputFileSystem = serverFs
+  const serverMiddleware = webpackDevMiddleware(serverCompiler, { index: false, writeToDisk: true })
+  koaServerMiddleware = async (ctx, next) => { await serverMiddleware(ctx.req, _res(ctx), next) }
+}
 
-const Koa = require('koa')
-const koaStatic = require('koa-static')
-const fs = require('fs')
-const app = new Koa()
+function startup({ PORT = 8080 } = {}) {
+  const app = new Koa()
+  if (__DEV__) {
+    app.use(koaClientMiddleware)
+    app.use(async function (ctx, next) {
+      ctx.clientLocals = ctx.res.locals
+      ctx.res.locals = undefined
+      await next()
+    })
+    app.use(koaServerMiddleware)
+    app.use(async function (ctx, next) {
+      ctx.serverLocals = ctx.res.locals
+      ctx.res.locals = undefined
+      await next()
+    })
+  } else {
+    const koaStatic = require('koa-static')
+    app.use(koaStatic(path.join(__dirname, '../dist'), {
+      defer: true
+    }))
+  }
+  app.use(async function (ctx, next) {
+    // const { default: Adapter, createStore } = require('../dist/server/index')
+    // const assetsByChunkName = ctx.state.webpackStats.toJson().assetsByChunkName;
+    // cfs.writeFile(path.join(__dirname,'../log/client.log.js'),JSON.stringify(ctx.clientLocals.webpackStats.toJson(),null,4))
+    // cfs.writeFile(path.join(__dirname,'../log/server.log.js'),JSON.stringify(ctx.serverLocals.webpackStats.toJson(),null,4))
 
-const React = require('react')
-const ReactDOMServer = require('react-dom/server');
-const { ChunkExtractor } = require('@loadable/server')
+    // const dist=path.resolve(__dirname,'../dist')
+    // const html=clientFs.readFileSync(path.resolve(dist,'index.html'),'utf-8')
+
+    const ext = path.extname(ctx.url)
+    if (!ext) {
+      await provideHtml(ctx)
+    }
+    // await next()
+  })
+
+  app.listen(PORT, function () {
+    console.log('listen at ' + PORT)
+  })
+}
 
 function getData() {
   return new Promise(resolve => {
@@ -132,3 +90,43 @@ function getData() {
   })
 }
 
+function getDist() {
+  return path.resolve(__dirname, '../dist')
+}
+
+async function provideHtml(ctx) {
+  const React = require('react')
+  const ReactDOMServer = require('react-dom/server');
+  const { ChunkExtractor } = require('@loadable/server')
+
+  // const modules = []
+
+  const nodeExtractor = new ChunkExtractor({
+    stats: require('../dist/server/loadable-stats.json'),
+    entrypoints: 'index',
+    publicPath: '/client/'
+  })
+
+  const { default: Adapter, createStore } = require('../dist/server/index')
+
+  const serverData = await getData()
+  const store = createStore({
+    saveData: serverData
+  })
+
+  const jsx = nodeExtractor.collectChunks(React.createElement(Adapter, { store, req: ctx.req }, null))
+
+  const dist = getDist()
+  let html = clientFs.readFileSync(path.resolve(dist, './index.html'), "utf-8")
+  html = html
+    .replace('<!-- reactComponents -->', ReactDOMServer.renderToString(jsx))
+    .replace('<!-- ssrData -->', JSON.stringify({ saveData: serverData }))
+    .replace('<!-- ssr_Css -->', nodeExtractor.getStyleTags())
+    .replace('<!-- ssr_Scirpts -->', nodeExtractor.getScriptTags())
+
+  ctx.status = 200
+  ctx.set('content-type', 'text/html')
+  ctx.body = html
+}
+
+startup()
